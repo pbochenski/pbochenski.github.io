d9b0a64395f6a885e1d5494156bc4915e200480e
Pawel Bochenski <pawel.bochenski@netguru.com>
2022-06-21 11:13:42 +0200

    72 image gallery update

--- generate_galery.sh
+++
@@ -1,1 +0,0 @@
-thumbsup --input "/Users/bochen/Downloads/galeria" --output "./gallery_build" --theme flow --sort-media-by filename --sort-media-direction desc --cleanup true --title "Paweł Bocheński" --footer ""
