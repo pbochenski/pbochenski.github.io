3942209181da9655d6fa9c8edb85fb0f6bcace11
Pawel Bochenski <pawel.bochenski@external.wolt.com>
2023-05-17 08:45:11 +0200

    post plener 3

--- generate_miniatures_for_post.py
+++ generate_miniatures_for_post.py
@@ -10,25 +10,51 @@ def get_arg(index):
         return ''
     else:
         return sys.argv[index]
+    
+def createMiniatures(post, fileName):
+    folder = "/Users/bochen/Documents/kod/pbochenski.github.io/assets/images/{0}/".format(post)
+    os.makedirs(folder, exist_ok=True)
+    fileCount = int(len(next(os.walk(folder))[2]) / 3)
+    newFileName = newFileName = '{0:03d}.jpg'.format(fileCount)
 
+    with Image(filename=fileName) as img:
+        with img.clone() as thumb:
+            thumb.resize(120, 120)
+            thumb.save(filename="{0}/thumb_{1}".format(folder, newFileName))
+        with img.clone() as small:
+            small.transform(resize="300^>")
+            small.save(filename="{0}/small_{1}".format(folder, newFileName))
+        with img.clone() as large:
+            large.transform(resize="1000^>")
+            large.save(filename="{0}/large_{1}".format(folder, newFileName))
+    
+    print("""<div
+            class="albumList"
+            data-sub-html=""
+            data-download-url="../assets/images/{1}/large_{0}"
+            data-src="../assets/images/{1}/large_{0}"
+            data-exthumbimage="../assets/images/{1}/thumb_{0}"
+            >
+            <a href="../assets/images/{1}/large_{0}">
+            <img src="../assets/images/{1}/small_{0}" height="300" />
+            </a>
+            </div>""".format(newFileName, post))
+
+print("<ul id=\"media\" class=\"clearfix justified-gallery\">")
 
 if get_arg(1) != '':
     if get_arg(2) != '':
-        fileName = get_arg(2)
-        post = get_arg(1)
+        path = get_arg(2)
+        postName = get_arg(1)
 
-        folder = "/Users/bochen/Documents/kod/pbochenski.github.io/assets/images/{0}/".format(post)
-        os.makedirs(folder, exist_ok=True)
-        fileCount = int(len(next(os.walk(folder))[2]) / 3)
-        newFileName = newFileName = '{0:03d}.jpg'.format(fileCount)
+        if os.path.isdir(path):
+            for root, dirs, files in os.walk(path):
+                for file in sorted(files):
+                    file_path = os.path.join(root, file)
+                    createMiniatures(postName,file_path)
+        else:
+            createMiniatures(path)
 
-        with Image(filename=fileName) as img:
-            with img.clone() as thumb:
-                thumb.resize(120, 120)
-                thumb.save(filename="{0}/thumb_{1}".format(folder, newFileName))
-            with img.clone() as small:
-                small.transform(resize="300^>")
-                small.save(filename="{0}/small_{1}".format(folder, newFileName))
-            with img.clone() as large:
-                large.transform(resize="1000^>")
-                large.save(filename="{0}/large_{1}".format(folder, newFileName))
+
+print("</ul>")
+        
